bc79b087abcd5223bb3c675c1210eeac853c7c43
shaosuo <shaosuo@youmi.net>
2019-01-31 15:12:33 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+// import Stream from 'Stream.react'
+// import Collection from 'Collect.react'
+
+// import './App.css'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <Stream className="Stream" />
   }
 }
 
-export default App;
+class Stream extends Component {
+  render() {
+    return <h1>hello</h1>
+  }
+}
+
+export default App
